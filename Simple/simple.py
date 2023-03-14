03b4da58e16259be5c250fee89239a23fa26bd16
Marina <nunezherediamarina@gmail.com>
2023-03-13 23:11:21 -0300

    Se quitó las llamadas de funciones de generar_lista_edades() y se agrego la llamada de funciones finales

--- Simple/simple.py
+++ Simple/simple.py
@@ -8,10 +8,6 @@ def generar_lista_edades():
         lista.append(diccionario)
     return lista
 
-lista_edades = generar_lista_edades()
-print(lista_edades)
-
-
 def ordenar_lista_edades(lista):
     lista_ordenada = sorted(lista, key=lambda x: x['edad'], reverse=True)
     id_persona_mas_joven = lista[-1]['id']
@@ -19,3 +15,7 @@ def ordenar_lista_edades(lista):
     print(f"La persona mas joven tiene el id {id_persona_mas_joven}")
     print(f"La persona mas vieja tiene el id {id_persona_mas_vieja}")
     return lista_ordenada
+
+lista_edades = generar_lista_edades()
+lista_ordenada = ordenar_lista_edades(lista_edades)
+print(lista_ordenada)
