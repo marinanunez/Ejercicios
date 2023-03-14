d998ffc89a673ea58583b7ae66ced4050e86882e
Marina <nunezherediamarina@gmail.com>
2023-03-14 00:37:51 -0300

    Instacia la clase con radio <=0. Excepción de error

--- Clases/clases.py
+++ Clases/clases.py
@@ -3,3 +3,11 @@ class Circulo:
         if radio <= 0:
             raise ValueError("El radio debe ser mayor que cero.")
         self._radio = radio
+
+def radio(self):
+    return self._radio
+
+def radio(self, valor):
+    if valor <= 0:
+        raise ValueError("El radio debe ser mayor que cero.")
+    self._radio = valor
