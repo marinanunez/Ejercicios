13c439570d6845ac2cf504a7b41e0874458b0ffa
Marina <nunezherediamarina@gmail.com>
2023-03-13 23:00:37 -0300

    Función que recibe lo generado en generar_lista_edades() y lo ordena de mayor a menor

--- Simple/simple.py
+++ Simple/simple.py
@@ -10,3 +10,12 @@ def generar_lista_edades():
 
 lista_edades = generar_lista_edades()
 print(lista_edades)
+
+
+def ordenar_lista_edades(lista):
+    lista_ordenada = sorted(lista, key=lambda x: x['edad'], reverse=True)
+    id_persona_mas_joven = lista[-1]['id']
+    id_persona_mas_vieja = lista[0]['id']
+    print(f"La persona mas joven tiene el id {id_persona_mas_joven}")
+    print(f"La persona mas vieja tiene el id {id_persona_mas_vieja}")
+    return lista_ordenada
