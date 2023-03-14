10e1cbea846d08b5b8f8e961e0f4e6e0cd497f46
Marina <nunezherediamarina@gmail.com>
2023-03-14 00:41:12 -0300

    Función que permite la multiplicación del círculo

--- Clases/clases.py
+++ Clases/clases.py
@@ -11,3 +11,14 @@ def radio(self, valor):
     if valor <= 0:
         raise ValueError("El radio debe ser mayor que cero.")
     self._radio = valor
+    
+def area(self):
+    return 2 * 3.14159 * self._radio
+
+def __repr__(self):
+    return f"Circulo de radio {self._radio}"
+
+def __mul__(self, n):
+    if n <= 0:
+        raise ValueError("El factor de multiplicación debe ser mayor que cero.")
+    return Circulo(self._radio * n) 
